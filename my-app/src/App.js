7a5af09e0fe14a9043fc21e0297bed0306aa0f68
Nikola <nikolakuljic345@gmail.com>
2024-04-12 12:13:23 +0200

    test

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import  Home from './Home/Home';
+
+function App() {
+
+
+
+  const naslov123="Nikola";
+  return <Home naslov={naslov123}></Home>
+
+
+   
+  
+}
+
+export default App;
